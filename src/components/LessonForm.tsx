71c00c17491eea6cb6e8c45918b44683a291e56f
suhyun.p69 <suhyun.p69@gmail.com>
2025-02-28 22:07:55 +0900

    시작시간, 종료시간
    npx shadcn@latest add calendar
    npx shadcn@latest add popover

--- src/components/LessonForm.tsx
+++ src/components/LessonForm.tsx
@@ -1,3 +1,6 @@
+"use client"
+
+import { useState } from "react"
 import * as React from "react"
 
 import { Button } from "@/components/ui/button"
@@ -19,22 +22,38 @@ import {
   SelectValue,
 } from "@/components/ui/select"
 
+import { format } from "date-fns"
+import { Calendar } from "@/components/ui/calendar"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import { cn } from "@/lib/utils"
+import { CalendarIcon } from "lucide-react"
+
 export function LessonForm() {
+
+  const [startDate, setStartDate] = useState<Date>()
+  const [endDate, setEndDate] = useState<Date>()
+  
   return (
     <Card className="w-[350px]">
       <CardHeader>
         <CardTitle>Create Lesson</CardTitle>
-        <CardDescription>Deploy your new lesson.</CardDescription>
+        <CardDescription>수업을 생성합니다.</CardDescription>
       </CardHeader>
       <CardContent>
         <form>
           <div className="grid w-full items-center gap-4">
 
+            {/* 타이틀 */}
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="title">Title</Label>
-              <Input id="title" placeholder="Name of your lesson" />
+              <Input id="name" placeholder="title of your lesson" />
             </div>
 
+            {/* 강사1 */}
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="instructor1">Instructor1</Label>
               <Select>
@@ -44,12 +63,12 @@ export function LessonForm() {
                 <SelectContent position="popper">
                   <SelectItem value="browny">브라우니</SelectItem>
                   <SelectItem value="kali">칼리</SelectItem>
-                  <SelectItem value="scarlett">스칼렛</SelectItem>
-                  <SelectItem value="diana">디아나</SelectItem>
+                  <SelectItem value="ㄴcarlett">스칼렛</SelectItem>
                 </SelectContent>
               </Select>
             </div>
 
+            {/* 강사2 */}
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="instructor2">Instructor2</Label>
               <Select>
@@ -59,10 +78,63 @@ export function LessonForm() {
                 <SelectContent position="popper">
                   <SelectItem value="browny">브라우니</SelectItem>
                   <SelectItem value="kali">칼리</SelectItem>
-                  <SelectItem value="scarlett">스칼렛</SelectItem>
-                  <SelectItem value="diana">디아나</SelectItem>
+                  <SelectItem value="ㄴcarlett">스칼렛</SelectItem>
                 </SelectContent>
               </Select>
+            </div>
+
+            {/* 시작일 */}
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="start_date">시작일</Label>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <Button
+                    variant={"outline"}
+                    className={cn(
+                      "w-full justify-start text-left font-normal",
+                      !startDate && "text-muted-foreground"
+                    )}
+                  >
+                    <CalendarIcon className="mr-2 h-4 w-4" />
+                    {startDate ? format(startDate, "yyyy-MM-dd") : "날짜를 선택하세요"}
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0">
+                  <Calendar
+                    mode="single"
+                    selected={startDate}
+                    onSelect={setStartDate}
+                    initialFocus
+                  />
+                </PopoverContent>
+              </Popover>
+            </div>
+
+            {/* 종료일 */}
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="end_date">종료일</Label>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <Button
+                    variant={"outline"}
+                    className={cn(
+                      "w-full justify-start text-left font-normal",
+                      !endDate && "text-muted-foreground"
+                    )}
+                  >
+                    <CalendarIcon className="mr-2 h-4 w-4" />
+                    {endDate ? format(endDate, "yyyy-MM-dd") : "날짜를 선택하세요"}
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0">
+                  <Calendar
+                    mode="single"
+                    selected={endDate}
+                    onSelect={setEndDate}
+                    initialFocus
+                  />
+                </PopoverContent>
+              </Popover>
             </div>
 
           </div>
